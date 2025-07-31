1ced748829c4227b14bf6ad8d4b2551850d6e420
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-07-31 10:51:38 -0300

    fist commit

--- src/Aula2/DataBank/EncapBank.java
+++ src/Aula2/DataBank/EncapBank.java
@@ -1,4 +1,21 @@
 package Aula2.DataBank;
 
 public class EncapBank {
+    private double saldo;
+    public double deposito;
+    public double saque;
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        }else  {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+    public void depositar(double valor) {
+        saldo += valor;
+    }
 }
