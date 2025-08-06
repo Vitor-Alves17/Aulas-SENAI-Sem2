d57b6f112d07886b1686cd148aad0a78c704afc9
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-08-06 10:37:58 -0300

    Update EncapBank.java

--- src/Aula2/DataBank/EncapBank.java
+++ src/Aula2/DataBank/EncapBank.java
@@ -1,4 +1,20 @@
 package Aula2.DataBank;
 
 public class EncapBank {
+
+  private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        }else  {
+            System.out.println("Saldo insuficiente");
+        }
+    }
 }
