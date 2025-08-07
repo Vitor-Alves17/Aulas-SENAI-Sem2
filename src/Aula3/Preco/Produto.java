fa55bc9f1e12c095b8c0d4a3b6b6e2b142db4212
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-08-07 08:49:45 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Aula2/DataBank/EncapBank.java

--- src/Aula3/Preco/Produto.java
+++ src/Aula3/Preco/Produto.java
@@ -1,6 +1,9 @@
 package Aula3.Preco;
 
+import java.util.Scanner;
+
 public class Produto {
+    Scanner ler = new Scanner(System.in);
 
     private String nome;
     private double preco;
@@ -9,13 +12,17 @@ public class Produto {
         this.nome = nome;
     }
     public void setPreco(double preco){
-        this.preco = preco;
+       while(preco < 0){
+           System.out.println("Valor inválido, digite novamente: ");
+           preco = ler.nextDouble();
+       }
+       this.preco = preco;
     }
 
     public void mostrar() {
         this.preco = preco;
         this.nome = nome;
-        System.out.println("Produto: " + nome + "\n" + "Preço: " + preco);
+        System.out.println("Produto: " + nome + "\n" + "Preço: " + String.format("%.2f", preco));
     }
 
     public void descontar(double valor) {
