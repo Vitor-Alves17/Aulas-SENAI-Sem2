ea8ab33cd03145288ef947ef82dd1fcfaa7f2a4e
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-08-28 08:34:05 -0300

    refactor: mudança nas classes

--- src/Aula8/Cache/Main.java
+++ src/Aula8/Cache/Main.java
@@ -1,16 +1,23 @@
 package Aula8.Cache;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    public static ArrayList<Pessoa> databank = new ArrayList<>();
+    public static Pessoa cache[] = new Pessoa[10];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Pessoa pessoa = new Pessoa();
+
 
         System.out.println("""
                 Bem-vindo ao simulador de cache! 
                 Digite 1 para iniciar: 
                 """);
-        Cache.mock();
-        Cache.veri();
+        pessoa.mock();
+        pessoa.veri();
     }
 }
